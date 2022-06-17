81331dc98c54c02f2344fa7e47b2b2eb75362a8e
AmarWayadande <amarwayadande3142.com>
2022-06-17 14:38:50 +0530

    src/main/java/com/example/demo/GitController.java v2

--- src/main/java/com/example/demo/GitController.java
+++ src/main/java/com/example/demo/GitController.java
@@ -1,8 +1,15 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GitController {
 
+    @GetMapping("/getData")
+    public String getData()
+    {
+
+       return "";
+    }
 }
